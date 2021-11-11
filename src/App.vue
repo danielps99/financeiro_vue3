0dd1b90a107739050cb5bdcf04ac6b9b788dc1b8
Daniel <danielps99@gmail.com>
2021-11-11 20:55:03 -0300

    Configurar bulma

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,8 @@
 <template>
   <div id="nav">
-      <router-link to="/"> Início </router-link>
-      <router-link to="/about"> Sobre </router-link>
-      <router-link to="/receber"> Configurações </router-link>
+      <router-link class="button is-primary is-rounded px-2 mx-2" to="/"> Início </router-link>
+      <router-link class="button is-primary is-rounded px-2 mx-2" to="/about"> Sobre </router-link>
+      <router-link class="button is-primary is-rounded px-2 mx-2" to="/receber">Configuração</router-link>
   </div>
   <router-view/>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
